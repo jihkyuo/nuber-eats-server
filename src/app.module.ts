9fe180e7a3eab7bf5085851133b993c57dbaeacd
지오현 <jihkyuoo@naver.com>
2023-07-09 21:58:53 +0900

    nest g mo restaurants 명령어 실행
    1. 생성된 모듈을 자동으로 app.module에 import 된다.
    2. resolver 파일을 만든 후, @Resolver (from nest/graphql) 데코레이터를 사용한다.
    3. 만든 resolver 파일은 1번에서 생성된 restaurants.module.ts 파일에 providers 로 import 한다.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,15 @@ import { Module } from '@nestjs/common';
 import { join } from 'path';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
+import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-
     }),
+    RestaurantsModule,
   ],
   controllers: [],
   providers: [],

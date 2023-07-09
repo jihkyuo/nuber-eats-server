74576d2b53590a3e249d1c3315be357ed4d9f4b0
지오현 <jihkyuoo@naver.com>
2023-07-09 23:37:26 +0900

    mutation 쿼리 인자의 validation 추가 npm i class-validator, class-transformer
    1. mutation 인자 타입을 정의한 dto 파일 클래스 필드에 validation 데코레이터를 추가한다.
    2. @IsString, @IsBoolean, @Length
    3. main.ts 파일에서 validation 파이프라인을 추가해야한다.
    app.useGlobalPipe(new ValidationPipe())

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,17 +1,23 @@
-import { ArgsType, Field, InputType } from '@nestjs/graphql';
+import { ArgsType, Field } from '@nestjs/graphql';
 import { Restaurant } from '../entities/restaurants.entity';
+import { IsBoolean, IsString, Length } from 'class-validator';
 
 @ArgsType()
 export class CreateRestaurantDto implements Restaurant {
   @Field(type => String)
+  @IsString()
   address: string;
 
   @Field(type => Boolean)
+  @IsBoolean()
   isVegan: boolean;
 
   @Field(type => String)
+  @IsString()
+  @Length(5, 10)
   name: string;
 
   @Field(type => String)
+  @IsString()
   ownerName: string;
 }

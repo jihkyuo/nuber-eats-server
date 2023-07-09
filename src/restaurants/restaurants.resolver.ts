b762472321a36a940fbfde9be9c8617071589e73
지오현 <jihkyuoo@naver.com>
2023-07-09 22:58:57 +0900

    @Args => 쿼리 함수에 받을 인자 데코레이터
    1. Resolver 함수에 정의한 쿼리 함수의 인자로 @Args 데코레이터를 사용하여 인자를 추가한다.
    2. graphql 플레이그라운드에 바로 반영되는 것을 확인할 수 있다.

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,10 +1,12 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurants.entity';
 
 @Resolver()
 export class RestaurantsResolver {
-  @Query(returns => Restaurant)
-  myRestaurant() {
-    return true; // todo Restaurant 를 return 해야함
+  @Query(returns => [ Restaurant ])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    console.log(veganOnly);
+
+    return [];
   }
 }

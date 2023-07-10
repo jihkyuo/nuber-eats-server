0f851e259c6e2d0ed913798a8d323c88d2c01710
지오현 <jihkyuoo@naver.com>
2023-07-10 23:27:09 +0900

    Joi 환경변수 유효성 검사
    DB_HOST: Joi.string().required(),
    DB_PORT: Joi.string().required(),
    DB_USERNAME: Joi.string().required(),
    DB_PASSWORD: Joi.string().required(),
    DB_NAME: Joi.string().required(),
    
    각각의 환경변수에 대해 string 타입이며, 필수라는 유효성 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,12 @@ import * as Joi from 'joi';
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
       ignoreEnvFile: process.env.NODE_ENV === 'prod',
       validationSchema: Joi.object({
-        NODE_ENV: Joi.string().valid('dev', 'prod'),
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
       }),
     }),
     TypeOrmModule.forRoot({

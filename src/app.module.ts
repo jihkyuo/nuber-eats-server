ab35e8773f1155ad5f5cc19fb5d9295857431254
지오현 <jihkyuoo@naver.com>
2023-07-10 22:47:23 +0900

    typeorm 설치 (DB와 연결하기 위한 패키지)
    1. app.module에 TypeOrmModule config 설정
    - 기본 port는 5432
    - DB type은 postgres 사용
    - password는 localhost에서는 아무 비번이나 입력해도 상관없음
    - database는 postico에 만든 사용할 DB 이름과 맞아야함
    - synchronize 는 현재 모듈의 상태로 마이그레이션할지 결정

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,23 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       driver: ApolloDriver,
       autoSchemaFile: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'jiohyeon',
+      password: 'test', // localhost 에서는 아무 비번이나 가능
+      database: 'nuber-eats',
+      synchronize: true,
+      logging: true,
     }),
     RestaurantsModule,
   ],

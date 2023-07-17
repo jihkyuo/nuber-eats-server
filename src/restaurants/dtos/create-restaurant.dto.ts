122f48546ec194fc60e63b0b0944778a11e82dba
지오현 <jihkyuoo@naver.com>
2023-07-17 18:30:39 +0900

    DTO에서 Restaurant entity를 가져와 쓸 때, InputType이 되기 위해 이전에 세팅을 했었는데, 두번째 방법은 entity 자체를 InputType이라고 선언하는 것이다.
    1. entity 데코레이터로 InputType을 추가한다.
    2. 인자로 isAbstract: true로 설정함으로써, 스키마에 포함시키지 않는다라고 한다.
    3. abstract 는 직접 사용하는 것이 아닌, 확장시킨다라는 의미이다.

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -2,5 +2,5 @@ import { InputType, OmitType } from '@nestjs/graphql';
 import { Restaurant } from '../entities/restaurants.entity';
 
 @InputType()
-export class CreateRestaurantDto extends OmitType(Restaurant, [ 'id' ], InputType) {
+export class CreateRestaurantDto extends OmitType(Restaurant, [ 'id' ]) {
 }

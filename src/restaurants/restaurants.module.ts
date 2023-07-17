d5953b2c8445b45bea4014717c2f732b3fcea398
지오현 <jihkyuoo@naver.com>
2023-07-17 11:42:46 +0900

    Active Record Active Record 패턴은 모델 내에서 데이터베이스에 액세스하는 접근 방식입니다.
    Data Mapper
    Data Mapper는 모델 대신 리포지토리 내의 데이터베이스에 액세스하는 접근 방식입니다.
    데이터 매퍼 접근 방식을 사용하여 "리포지토리"라는 별도의 클래스에서 모든 쿼리 메서드를 정의하고 리포지토리를 사용하여 객체를 저장, 제거 및 로드합니다.
    데이터 매퍼에서 엔터티는 매우 멍청합니다.객체는 속성을 정의하고 일부 "더미" 메서드가 있을 수 있습니다.
    
    - restaurants 모듈에서 사용하기 위해서 Restaurants entity를 imports 한다.

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RestaurantsResolver } from './restaurants.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Restaurant } from './entities/restaurants.entity';
 
 @Module({
+  imports: [ TypeOrmModule.forFeature([ Restaurant ]) ],
   providers: [ RestaurantsResolver ],
 })
 export class RestaurantsModule {

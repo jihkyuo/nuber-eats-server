0e18ade5ba6f6da987c67ee0472e4677a03cd8b9
지오현 <jihkyuoo@naver.com>
2023-07-17 11:57:57 +0900

    service에 DB inject

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { Restaurant } from './entities/restaurants.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class RestaurantService {
+  constructor(
+    @InjectRepository(Restaurant) private readonly restaurants: Repository<Restaurant>,
+  ) {
+  }
 
   getAll(): Restaurant[] {
     return [];
